abfb5f8f700ce4bb5124fc026db9b77f9453478a
dzh <1002837527@qq.com>
2021-07-30 04:03:26 -0700

    first

--- commen/yaml_url.py
+++ commen/yaml_url.py
@@ -7,14 +7,14 @@ import yaml
 class YamlUrl:
     # 获取get_login.yaml内容
     def read_login_yaml(self, yaml_name):
-        with open('/Users/dingzhihui/dzh_test/PycharmProjects/hq_pytest_allure_yaml/testCase/test_hq_passport/'+yaml_name, mode='r', encoding='utf-8')as f:
+        with open('/Users/dingzhihui/dzh_test/PycharmProjects/hq_pytest_allure/testCase/test_hq_passport/'+yaml_name, mode='r', encoding='utf-8')as f:
             # print(os.getcwd()+'/'+yaml_name)
             value = yaml.load(stream=f, Loader=yaml.FullLoader)
             return value
 
-    # 获取get_login.yaml内容
+    # 写入get_login.yaml内容
     def write_token_yaml(self, yaml_name):
-        with open('/Users/dingzhihui/dzh_test/PycharmProjects/hq_pytest_allure_yaml/commen/yaml_url.py', mode='r', encoding='utf-8')as f:
+        with open('/Users/dingzhihui/dzh_test/PycharmProjects/hq_pytest_allure/commen/yaml_url.py', mode='r', encoding='utf-8')as f:
             # print(os.getcwd()+'/'+yaml_name)
             value = yaml.load(stream=f, Loader=yaml.FullLoader)
         return value
